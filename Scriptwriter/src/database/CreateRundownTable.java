870eeebbfa72ab7b05ac9aab602d14728b33cae1
docplasma <thedocplasma@gmail.com>
2013-09-25 10:31:11 -0500

    Continuing work.

--- Scriptwriter/src/database/CreateRundownTable.java
+++ Scriptwriter/src/database/CreateRundownTable.java
@@ -1,22 +1,47 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import data.Story;
+
 public class CreateRundownTable {
 
 	
 	
-	CreateRundownTable(String tableName) {
+	CreateRundownTable(String tableName, Story story) {
 		
 		final String DRIVER = "com.mysql.jdbc.Driver";
-		final String CONNECTION_HEAD = "jdbc:mysql://localhost/";
-		final String CONNECTION_TAIL = ";create=true";
+		final String CONNECTION_HEAD = "jdbc:mysql://";
+		final String CONNECTION_TAIL = "/;create=true";
+		String connectionAddress = "localhost";
 		
-		String Connection = CONNECTION_HEAD + tableName + CONNECTION_TAIL;
+		String Connection = CONNECTION_HEAD + connectionAddress + tableName + CONNECTION_TAIL;
 		try {
-			
+			Class.forName(DRIVER).newInstance();
 		}
-		catch {
-			
+		catch (InstantiationException e) {
+			e.printStackTrace();
 		}
-		
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try (Connection connection = DriverManager.getConnection(Connection); Statement statement = connection.createStatement()) {
+			statement.executeUpdate("create table " + tableName +
+					" (Story VARCHAR(32) NOT NULL PRIMARY KEY, " +
+					"Type VARCHAR(32), " +
+					"Note VARCHAR(32) " +
+					"STime TIME() " +
+					"OTime TIME())");
+			statement.executeUpdate("insert into " + tableName + " ");
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
