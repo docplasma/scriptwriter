b985276d199ecf8cfceb1461db4607c97b2238c5
docplasma <thedocplasma@gmail.com>
2013-09-27 10:11:32 -0500

    Implemented insert.

--- Scriptwriter/src/database/RundownTableDriver.java
+++ Scriptwriter/src/database/RundownTableDriver.java
@@ -44,9 +44,12 @@ public class RundownTableDriver {
 			e.printStackTrace();
 		}
 		try (Connection con = DriverManager.getConnection(connection); Statement statement = con.createStatement()) {
-			statement.executeUpdate("CREATE TABLE IF NOT EXISTS `" + databaseName +"`.`" + tableName + "` (`StoryIndex` INT(11) NOT NULL," +
-					" `Story` TEXT NULL DEFAULT NULL, `Type` TEXT NULL DEFAULT NULL, `Note` TEXT NULL DEFAULT NULL," + 
-					" `STime` TIME NULL DEFAULT NULL, `OTime` TIME NULL DEFAULT NULL, PRIMARY KEY (`StoryIndex`))");
+			statement.executeUpdate("CREATE TABLE IF NOT EXISTS `" + databaseName +"`.`" + tableName + "` (`StoryIndex` INT(11) NOT NULL," + 
+					" `title` TEXT NULL DEFAULT NULL, `storyType` TEXT NULL DEFAULT NULL, `cameraIntroShot` TEXT NULL DEFAULT NULL," + 
+					" `cameraTagShot` TEXT NULL DEFAULT NULL, `cameraIntroNumber` INT NULL DEFAULT NULL," + 
+					" `cameraTagNumber` INT NULL DEFAULT NULL, `graphicsInfo` TEXT NULL DEFAULT NULL, `notes` TEXT NULL DEFAULT NULL," + 
+					" `intro` TEXT NULL DEFAULT NULL, `tag` TEXT NULL DEFAULT NULL, `storyDuration` TIME NULL DEFAULT NULL," + 
+					" `mediaDuration` TIME NULL DEFAULT NULL, PRIMARY KEY (`StoryIndex`))");
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
@@ -54,7 +57,37 @@ public class RundownTableDriver {
 	}
 	
 	//-----Methods
-	public void insertStory(Story story) {
-		//TODOs
+	public void insertStory(Story story, String tableName) {
+		
+		connectionAddress = "//localhost/";
+		databaseName = "scriptwriter";
+		
+		String connection = CONNECTION_HEAD + connectionAddress + databaseName + "?user=" + USER + "&password=" + PASS;
+	
+		try {
+			Class.forName(DRIVER).newInstance();
+		}
+		catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try (Connection con = DriverManager.getConnection(connection); Statement statement = con.createStatement()) {
+			statement.executeUpdate("INSERT INTO " + tableName + " (title, storyType, cameraIntroShot, cameraTagShot, cameraIntroNumber," + 
+					" cameraTagNumber, graphicsInfo, notes, intro, tag, STime, OTime)" +
+					" VALUES ('" + story.getTitle() + "', '" + story.getStoryType() + "', '" + story.getCameraIntroShot() + "'," +
+					story.getCameraTagShot() +  "', '" + story.getCameraIntroNumber() + "', '" + story.getCameraTagNumber() + "'," +
+					story.getGraphicsInfo() + "', '" + story.getNotes() + "', '" + story.getIntro() + "', '" + story.getTag() +
+					story.getStoryDuration() + "', '" + story.getMediaDuration() + "', )");
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
