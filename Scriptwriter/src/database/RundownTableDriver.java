87d0553a222937c86d574759d692efd55acdd6bc
docplasma <thedocplasma@gmail.com>
2013-09-27 16:22:59 -0500

    Organized Imorts

--- Scriptwriter/src/database/RundownTableDriver.java
+++ Scriptwriter/src/database/RundownTableDriver.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Time;
-
 import org.joda.time.Duration;
 
 import data.Story;
