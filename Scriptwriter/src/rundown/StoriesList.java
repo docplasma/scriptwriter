3422a05ce4cdb4acb4105b6d71137b76b2668b2e
docplasma <thedocplasma@gmail.com>
2013-10-08 12:23:38 -0500

    Created constructor that accepts rundown name. Created method to refresh rundown.

---
+++ Scriptwriter/src/rundown/StoriesList.java
@@ -0,0 +1,110 @@
+package rundown;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import data.Story;
+import database.RundownTableDriver;
+
+public class StoriesList extends JPanel {
+
+	//-----Data Members
+	String rundown;
+	RundownTableDriver driver;
+	
+	//-----Constructors
+	StoriesList() {
+		super(new GridLayout(1,0));
+		//-----Data Members
+		driver = new RundownTableDriver("Nightly_9-17-19");
+		Object[][] data = {
+			    {"SQUEEZE", "PKG", "", "00:00", "00:00"},
+			    {"OPEN", "LIVE", "", "00:00", "00:00"},
+			    {"TROYTAILGATE", "PKG", "", "00:00", "00:00"},
+			    {"DODGEBALL", "PKG", "", "00:00", "00:00"},
+			    {"PINGPONG", "PKG", "", "00:00", "00:00"},
+			    {"STATE", "LIVE", "", "00:00", "00:00"},
+			    {"TEASE", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTOGLOBAL", "LIVE", "", "00:00", "00:00"},
+			    {"WILDFIRES", "PKG", "", "00:00", "00:00"},
+			    {"SYRIA", "PKG", "", "00:00", "00:00"},
+			    {"TOSSFROMGLOBAL", "LIVE", "", "00:00", "00:00"},
+			    {"TOSSTOSPORTS", "LIVE", "", "00:00", "00:00"},
+			    {"FOOTBALL", "PKG", "", "00:00", "00:00"},
+			    {"SOCCER", "PKG", "", "00:00", "00:00"},
+			    {"CROSSCOUNTRY", "PKG", "", "00:00", "00:00"},
+			    {"ITSTTIME", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTONEWS", "LIVE", "", "00:00", "00:00"},
+			    {"TEASE2", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTOWEATHER", "LIVE", "", "00:00", "00:00"},
+			    {"WEATHER", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTONEWS", "LIVE", "", "00:00", "00:00"},
+			    {"TTALK", "PKG", "", "00:00", "00:00"},
+			    {"CLOSE", "LIVE", "", "00:00", "00:00"}
+			};
+		String[] columnNames = {"Story", "Type", "Note", "Time", "Show Time"};
+		//Story story = new Story();
+		
+		
+		//-----GUI Constructors
+		
+		JTable stories = new JTable(data, columnNames);
+		stories.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		stories.setFillsViewportHeight(true);
+		JScrollPane tableScrollPane = new JScrollPane(stories);
+		add(tableScrollPane);
+	}
+	
+	StoriesList(String rundown) {
+		super(new GridLayout(1,0));
+		//-----Data Members
+		this.rundown = rundown;
+		driver = new RundownTableDriver(this.rundown);
+		Object[][] data = {
+			    {"SQUEEZE", "PKG", "", "00:00", "00:00"},
+			    {"OPEN", "LIVE", "", "00:00", "00:00"},
+			    {"TROYTAILGATE", "PKG", "", "00:00", "00:00"},
+			    {"DODGEBALL", "PKG", "", "00:00", "00:00"},
+			    {"PINGPONG", "PKG", "", "00:00", "00:00"},
+			    {"STATE", "LIVE", "", "00:00", "00:00"},
+			    {"TEASE", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTOGLOBAL", "LIVE", "", "00:00", "00:00"},
+			    {"WILDFIRES", "PKG", "", "00:00", "00:00"},
+			    {"SYRIA", "PKG", "", "00:00", "00:00"},
+			    {"TOSSFROMGLOBAL", "LIVE", "", "00:00", "00:00"},
+			    {"TOSSTOSPORTS", "LIVE", "", "00:00", "00:00"},
+			    {"FOOTBALL", "PKG", "", "00:00", "00:00"},
+			    {"SOCCER", "PKG", "", "00:00", "00:00"},
+			    {"CROSSCOUNTRY", "PKG", "", "00:00", "00:00"},
+			    {"ITSTTIME", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTONEWS", "LIVE", "", "00:00", "00:00"},
+			    {"TEASE2", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTOWEATHER", "LIVE", "", "00:00", "00:00"},
+			    {"WEATHER", "PKG", "", "00:00", "00:00"},
+			    {"TOSSTONEWS", "LIVE", "", "00:00", "00:00"},
+			    {"TTALK", "PKG", "", "00:00", "00:00"},
+			    {"CLOSE", "LIVE", "", "00:00", "00:00"}
+			};
+		String[] columnNames = {"Story", "Type", "Note", "Time", "Show Time"};
+		
+		
+		//Story story = new Story();
+		
+		
+		//-----GUI Constructors
+		
+		JTable stories = new JTable(data, columnNames);
+		stories.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		stories.setFillsViewportHeight(true);
+		JScrollPane tableScrollPane = new JScrollPane(stories);
+		add(tableScrollPane);
+	}
+	
+	//-----Methods	
+	void refreshRundown() {
+		
+	}
+}
